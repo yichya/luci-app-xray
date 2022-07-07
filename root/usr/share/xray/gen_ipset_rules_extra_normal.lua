7f6b9c68a031b4eb99f3ce60589d216a8f818c88
ttimasdf <opensource@rabit.pw>
2022-07-07 09:07:07 +0800

    Update special purpose IP addresses from RFC5735

--- root/usr/share/xray/gen_ipset_rules_extra_normal.lua
+++ root/usr/share/xray/gen_ipset_rules_extra_normal.lua
@@ -8,12 +8,15 @@ add tp_spec_dst_sp 127.0.0.0/8
 add tp_spec_dst_sp 169.254.0.0/16
 add tp_spec_dst_sp 172.16.0.0/12
 add tp_spec_dst_sp 192.0.0.0/24
-add tp_spec_dst_sp 192.31.196.0/24
-add tp_spec_dst_sp 192.52.193.0/24
+add tp_spec_dst_sp 192.0.2.0/24
 add tp_spec_dst_sp 192.88.99.0/24
 add tp_spec_dst_sp 192.168.0.0/16
-add tp_spec_dst_sp 192.175.48.0/24
-add tp_spec_dst_sp 224.0.0.0/3]]
+add tp_spec_dst_sp 198.18.0.0/15
+add tp_spec_dst_sp 198.51.100.0/24
+add tp_spec_dst_sp 203.0.113.0/24
+add tp_spec_dst_sp 224.0.0.0/4
+add tp_spec_dst_sp 233.252.0.0/24
+add tp_spec_dst_sp 240.0.0.0/4]]
 
 return function(proxy)
     print(special_purpose_rules)
